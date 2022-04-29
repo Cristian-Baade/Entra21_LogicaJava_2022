2ac13c5da0f89c95a0556efb4453851d423fd559
Cristian-Baade <Schauffertcristian@gmail.com>
2022-04-29 00:35:18 -0300

    Terminei os Exercicios de Logica,racionalidade e Igualdade

---
+++ Aula04 - ExercicioOperadorIgualdade/README.md
@@ -0,0 +1,14 @@
+# aprendendo sobre operadores lógicos
+
+## tipos
+
+> - E AND && = comparação entre booleans mais restritivos
+
+ | A | B | RESULTADO |
+ |---|---|-----------|
+ | T | T |     T     |
+ | T | F |     F     |
+ | F | T |     F     |
+ | F | F |     F     |
+ 
+> - OU OR || = (comparação entre booleans) mais inclusivo conforme  
---
+++ Aula04 - ExercicioOperadorRelacional/src/br/com/entra21/operador/relacional/Main.java
@@ -0,0 +1,50 @@
+package br.com.entra21.operador.relacional;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Exercicio de Operador Relacional");
+		System.out.println("Faça um programa que informe se o 1º numero é maior que o 2º numero");
+		
+		byte idadeA=47,idadeB=69;
+		boolean resultado;
+		
+		resultado = idadeA > idadeB;
+		
+		System.out.println(idadeA+" é (>) que "+idadeB+ " ? " +resultado);
+	
+		//-----------------------------------
+		
+		float pesoA=73, pesoB=72;
+		
+		System.out.println("Faça um programa que informe se o 1º numero é maior ou igual que o 2º numero");
+		
+		resultado = pesoA >= pesoB;
+		
+		System.out.println(pesoA+ " é (>=) que " +pesoB+ " ? " +resultado);
+		
+		//------------------------------------
+		
+		int alturaA= 170, alturaB=160;
+		
+		System.out.println("Faça um programa que informe se o 1º numero é menor que o 2º numero");
+		
+		resultado = alturaA < alturaB;
+		
+		System.out.println(alturaA+ " é (<) que " +alturaB+ " ? " +resultado);
+		
+		//-------------------------------------
+		
+		double MetroA= 7.05, MetroB=7.05;
+		
+		System.out.println("Faça um programa que informe se o 1º numero é menor ou igual que o 2º numero");
+		
+		resultado = MetroA <=MetroB;
+		
+	   System.out.println(MetroA+ " é (<=) uqe " +MetroB+ " ? " +resultado);
+		
+	}
+
+}
