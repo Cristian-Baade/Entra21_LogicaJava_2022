026540c89c5085defe62f4131e419b86236845ff
Cristian-Baade <Schauffertcristian@gmail.com>
2022-04-29 21:19:59 -0300

    Else if

---
+++ Aula05 - 04 - elseif/src/br/com/entra21/elseif/Main.java
@@ -0,0 +1,36 @@
+package br.com.entra21.elseif;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Aprendendo sobre else if (ou seja renovando o if antes do else )");
+	
+	float altura=1.7f;
+	double dinheiro=3000;
+	
+	if(altura<1.5)  {//é baixinho ?
+		System.out.println("é bem baixinho");
+	}else if(altura<=1.9f) {//é alto?
+		System.out.println("é alto");
+	}else {
+		//mesmo que eu não queira fazer nada aqui, é uma boa prática ter o else
+       }
+
+	
+	
+	//se ate 1200 . ate 1800 , ate 2000, qualquer valor acima
+	if(dinheiro <1200) {
+		System.out.println("salario minimo");
+	}else if(dinheiro<=1800) {
+		
+		System.out.println("ta ficando rico");
+	}else if(dinheiro<=2000) {
+		
+    System.out.println("me empresta :)");
+	}else {
+		System.out.println("me contrata vai");
+	}
+}
+}
